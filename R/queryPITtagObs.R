e03dd3ea8bb8993979fb8b4c1424a4d79f9e8b30
Kevin See <Kevin@qcinc.org>
2017-05-19 10:56:21 -0700

    created two functions to query DART for PIT tags crossing LGR, and to use those tags to estimate trap rate.

---
+++ R/queryPITtagObs.R
@@ -0,0 +1,108 @@
+#' @title PIT tag data
+#'
+#' @description Query and download data about PIT tags observed crossing a particular site, using DART.
+#'
+#' @author Kevin See
+#'
+#' @param site The site to query observations for.
+#' @param spp species to query window counts for. Possible choices are: Chinook, Coho, Steelhead, Sockeye
+#' @param yr year to query for window counts.
+#' @param start_day date (\code{month / day}) when query should start
+#' @param end_day date (\code{month / day}) when query should end
+#' @param span_yrs Should the query span mulitple years? If \code{NULL}, default value is chosen based on species.
+#'
+#' @source \url{http://www.cbr.washington.edu/dart}
+#'
+#' @import lubridate readr httr dplyr
+#' @export
+#' @return NULL
+#' @examples queryPITtagObs(yr = 2013)
+
+queryPITtagObs = function(site = 'GRA',
+                          spp = c('Chinook', 'Coho', 'Steelhead', 'Sockeye'),
+                          yr = NULL,
+                          start_day = NULL,
+                          end_day = NULL,
+                          span_yrs = NULL) {
+
+  # need a year
+  stopifnot(!is.null(yr))
+
+  if(site == 'GRA') proj = 'GRA:Lower Granite Dam Adult Fishway (GRA) rkm 522.173'
+
+  # default values
+  spp = match.arg(spp)
+
+  # match up species code with species name
+  spp_code_df = data.frame(Species = c('Chinook', 'Coho', 'Steelhead', 'Sockeye'),
+                           code = 1:4)
+
+  spp_code = spp_code_df$code[match(spp, spp_code_df$Species)]
+
+  # set up default start and end days
+  if(site == 'GRA' & spp == 'Chinook' & is.null(start_day)) start_day = '03/01'
+  if(site == 'GRA' & spp == 'Chinook' & is.null(end_day)) end_day = '08/17'
+
+  if(site == 'GRA' & grepl('Steelhead', spp) & is.null(start_day)) start_day = '07/01'
+  if(site == 'GRA' & grepl('Steelhead', spp) & is.null(end_day)) end_day = '06/30'
+
+  if(is.null(span_yrs)) {
+    if(spp == 'Steelhead') span_yrs = T
+    if(spp != 'Steelhead') span_yrs = F
+  }
+
+
+  # assign user agent to the GitHub repo for this package
+  ua = httr::user_agent('https://github.com/KevinSee/damEscapement')
+
+  # compose url with query
+  url_req = 'http://www.cbr.washington.edu/dart/cs/php/rpt/pitall_obs_de.php?'
+
+  # build query for DART
+  queryList = list(sc = 1,
+                   queryName = 'pit_obs_de',
+                   stage = 'A',
+                   outputFormat = 'csv',
+                   year = yr,
+                   proj = proj,
+                   species = spp_code,
+                   run = NULL,
+                   rear_type = NULL,
+                   span = ifelse(span_yrs, 'yes', 'no'),
+                   startdate = start_day,
+                   enddate = end_day,
+                   reltype = 'alpha',
+                   summary = 'yes')
+
+
+  if(span_yrs) {
+    queryList[['span']] = 'yes'
+    queryList = c(queryList,
+                  list(syear = yr - 1,
+                       eyear = yr))
+  }
+
+  # send query to DART
+  web_req = httr::GET(url_req,
+                      ua,
+                      query = queryList)
+
+  # if any problems
+  httr::stop_for_status(web_req,
+                        task = 'query data from DART')
+
+  # parse the response
+  parsed = httr::content(web_req,
+                         'text',
+                         encoding = 'ISO-8859-1') %>%
+    read_delim(delim = ',',
+               col_names = T) %>%
+    filter(!is.na(`Tag ID`)) %>%
+    mutate(Year = as.integer(Year))
+
+  names(parsed) = gsub(' ', '', names(parsed))
+
+  return(parsed)
+}
+
+
---
+++ R/tagTrapRate.R
@@ -0,0 +1,85 @@
+#' @title PIT tag based trap rate
+#'
+#' @description Estimate the trap rate at Lower Granite by using the proportion of previously PIT tagged fish observed pass a location that were found in the fish trap at that location.
+#'
+#' @author Kevin See
+#'
+#' @param trap_dataframe dataframe containing data from the fish trap, including columns named \code{Tag.ID}, \code{CollectionDate} and \code{SRR}.
+#' @param week_strata vector of intervals delimiting the weekly strata to summarize mark-recapture data over, as returned by the \code{weeklyStrata} function.
+#' @param mark_site PTAGIS code for the site at the location being queried for trap rate. This is used to filter out fish that were marked at the fish trap.
+#'
+#' @import lubridate dplyr
+#' @export
+#' @return NULL
+
+tagTrapRate = function(trap_dataframe = NULL,
+                      week_strata = NULL,
+                      mark_site = 'LGRLDR') {
+
+
+  # query PIT tag detections at Lower Granite
+  pit_gra = bind_rows(queryPITtagObs(spp = 'Chinook',
+                                     yr = year(int_start(week_strata[length(week_strata)])),
+                                     start_day = paste(month(int_start(week_strata[1])),
+                                                       mday(int_start(week_strata[1])),
+                                                       sep = '/'),
+                                     end_day = paste(month(int_end(week_strata[length(week_strata)])),
+                                                     mday(int_end(week_strata[length(week_strata)])),
+                                                     sep = '/'),
+                                     span_yrs = ifelse(year(int_start(week_strata[length(week_strata)])) - year(int_start(week_strata[1])) == 0, F, T)),
+                      queryPITtagObs(spp = 'Steelhead',
+                                     yr = year(int_start(week_strata[length(week_strata)])),
+                                     start_day = paste(month(int_start(week_strata[1])),
+                                                       mday(int_start(week_strata[1])),
+                                                       sep = '/'),
+                                     end_day = paste(month(int_end(week_strata[length(week_strata)])),
+                                                     mday(int_end(week_strata[length(week_strata)])),
+                                                     sep = '/'),
+                                     span_yrs = ifelse(year(int_start(week_strata[length(week_strata)])) - year(int_start(week_strata[1])) == 0, F, T))) %>%
+    arrange(ObsTime)
+
+
+  # filter out tags that were put in at Lower Granite
+  gra_tags = pit_gra %>%
+    filter(MarkSite == mark_site) %>%
+    select(TagID) %>%
+    distinct() %>%
+    as.matrix() %>%
+    as.character()
+
+  trap_rate_mr = trap_dataframe %>%
+    filter(!Tag.ID %in% gra_tags,
+           !is.na(Tag.ID)) %>%
+    select(TagID = Tag.ID, TrapDate = CollectionDate, SRR) %>%
+    full_join(pit_gra %>%
+                filter(!TagID %in% gra_tags) %>%
+                select(TagID, SpRRT, MarkSite, ReleaseDate, matches('Time'))) %>%
+    mutate(Species = ifelse(grepl('^1', SpRRT) | grepl('^1', SRR),
+                            'Chinook',
+                            ifelse(grepl('^3', SpRRT) | grepl('^3', SRR),
+                                   'Steelhead', NA))) %>%
+    mutate(ObsDate = floor_date(ObsTime, unit = 'days'),
+           diff = as.numeric(difftime(ObsDate, TrapDate, units = 'days'))) %>%
+    mutate(modDate = if_else(!is.na(TrapDate), TrapDate, ObsDate)) %>%
+    # mutate(modDate = if_else(abs(diff) < 2, modDate, floor_date(MaxTime, unit = 'days'))) %>%
+    mutate(inTrap = ifelse(!is.na(TrapDate), T, F))
+
+  # assign week number
+  trap_rate_mr$week_num = NA
+  for(i in 1:length(week_strata)) {
+    trap_rate_mr$week_num[with(trap_rate_mr, which(ymd(modDate) %within% week_strata[i]))] = i
+  }
+
+  prop_rate = trap_rate_mr %>%
+    filter(!is.na(ObsTime)) %>%
+    group_by(week_num) %>%
+    summarise(n_trap = sum(inTrap),
+              n_tot = n_distinct(TagID),
+              rate = n_trap / n_tot,
+              rate_se = sqrt((rate * (1 - rate)) / n_tot),
+              rate_cv = rate_se / rate)
+
+  return(prop_rate)
+
+}
+
