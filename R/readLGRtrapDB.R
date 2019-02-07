31b1ebb177d472d2af682c7239f8f947f7d7fbb9
Kevin See <Kevin@qcinc.org>
2019-02-07 13:08:49 -0800

    changed to using read_csv (from read.csv) to avoid date formatting issues caused by opening a file in Excel before reading into R. Also added a few lines to deal with NULL values in date_range

--- R/readLGRtrapDB.R
+++ R/readLGRtrapDB.R
@@ -7,7 +7,7 @@
 #' @param trap_path file path where a csv file containing the data from the fish trap is located
 #' @param date_range vector of length 2, with minimum and maximum dates to read.
 #'
-#' @import lubridate dplyr
+#' @import readr lubridate dplyr
 #' @export
 #' @return NULL
 #' @examples # do not run; #readLGRtrapDB()
@@ -19,16 +19,21 @@ readLGRtrapDB = function(trap_path = NULL,
     stop('File path to trap database needed.')
   }
 
-  lgr_trap = read.csv(trap_path) %>%
-    tbl_df() %>%
+  lgr_trap = readr::read_csv(trap_path) %>%
     rename(Tag.ID = LGDNumPIT) %>%
-    mutate(Date = floor_date(ymd_hms(CollectionDate), unit = 'day'),
+    mutate(Date = lubridate::floor_date(CollectionDate, unit = 'day'),
            SppCode = LGDSpecies,
            Tag.ID = as.character(Tag.ID),
            Tag.ID = ifelse(nchar(Tag.ID) < 3, NA, Tag.ID),
            # try to correct some incorrect species codes
            SppCode = ifelse(LGDSpecies != PtagisSpecies & GenSpecies %in% c(1, 3), as.integer(as.character(GenSpecies)), SppCode),
-           Species = ifelse(SppCode == 1, 'Chinook', ifelse(SppCode == 3, 'Steelhead', NA))) %>%
+           Species = ifelse(SppCode == 1, 'Chinook', ifelse(SppCode == 3, 'Steelhead', NA)))
+
+  if(is.null(date_range)) {
+    date_range = range(lgr_trap$Date)
+  }
+
+  lgr_trap = lgr_trap %>%
     # drop data from other species, and other runs of Chinook
     filter(Species %in% c('Chinook', 'Steelhead'),
            # !(Species == 'Chinook' & Date > lubridate::ymd(paste0(lubridate::year(Date), '0817'))),
@@ -39,6 +44,7 @@ readLGRtrapDB = function(trap_path = NULL,
            LGDLifeStage == 'RF',
            # drop sort-by-code fish
            (PTAgisSxCGRAObs != 'Yes' | is.na(PTAgisSxCGRAObs) ) )
+
 
   return(lgr_trap)
 }
