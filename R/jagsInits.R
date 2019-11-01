d112870a616deb27a81c96ba31a6de3873b450d8
Kevin See <kevin.see@biomark.com>
2019-10-31 17:12:09 -0700

    added ability in jagsInits.R to specify which parameters to return initial values for.

--- R/jagsInits.R
+++ R/jagsInits.R
@@ -5,11 +5,24 @@
 #' @author Kevin See
 #'
 #' @param jags_data list of data being passed to \code{JAGS} model
+#' @param params vector describing which parameters should have an initial value function applied to them. Currently accepts one or more of \code{X.log.all}, \code{trap.rate.true}. Default is all of them.
 #'
 #' @export
 #' @return NULL
 #'
-jagsInits = function(jags_data) {
-  return(list('X.log.all' = log(jags_data$Y.window + 1),
-              'trap.rate.true' = with(jags_data, trap.alpha / (trap.alpha + trap.beta) + 0.0001)))
+jagsInits = function(jags_data,
+                     params = c('X.log.all',
+                                'trap.rate.true')) {
+  init_list = NULL
+  if('X.log.all' %in% params) {
+    init_list = c(init_list,
+                  list('X.log.all' = log(jags_data$Y.window + 1)))
+  }
+
+  if('trap.rate.true' %in% params) {
+    init_list = c(init_list,
+                  list('trap.rate.true' = with(jags_data, trap.alpha / (trap.alpha + trap.beta) + 0.0001)))
+  }
+
+  return(init_list)
 }
